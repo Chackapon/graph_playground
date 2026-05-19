57807918c9db73f4c94ba758356b0917c3528e55
Chackapon <mateusz.miliutin@gmail.com>
2026-05-19 18:29:44 +0200

    - Added class body

--- include/GraphSearchAlgorithm.hpp
+++ include/GraphSearchAlgorithm.hpp
@@ -5,6 +5,9 @@
 #ifndef ALGOSY2GRAFY_GRAPHSEARCHALGORITHM_HPP
 #define ALGOSY2GRAFY_GRAPHSEARCHALGORITHM_HPP
 
+#include <unordered_map>
+#include <vector>
+
 #include "BaseGraph.hpp"
 
 enum search_mode {
@@ -14,6 +17,27 @@ enum search_mode {
 template <typename T, typename G> // node type, graph type
 requires Graph<G, T>
 class GraphSearchAlgorithm {
+    protected:
+    G& graph;
+    std::unordered_map<T, bool> visited;
+    public:
+    virtual ~GraphSearchAlgorithm() = default;
+    explicit GraphSearchAlgorithm( G& g ) : graph(g) {
+    } ;
+
+    std::vector<T> preorder;
+    std::vector<T> postorder;
+    std::unordered_map<T, T> parent; // drzewo DFS
+
+    virtual void run(T u) { visit(u); }
+    virtual void run() {
+        for ( auto n_it : graph.nodes() ) {
+            if (!visited[n_it])
+                visit(n_it);
+        }
+    }
+    virtual void visit(T u, int lvl = 0) = 0;
+    virtual void display(search_mode mode = PREORDER) = 0;
 };
 
 
