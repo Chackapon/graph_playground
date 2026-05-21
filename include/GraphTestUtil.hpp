b4e9d470136cf5db90e6d8e9a23063f88ed412e2
Chackapon <mateusz.miliutin@gmail.com>
2026-05-21 16:46:05 +0200

    - Separated functions i often use to generate graphs for tests into a separate header file and updated BFS and DFS file to use it

---
+++ include/GraphTestUtil.hpp
@@ -0,0 +1,133 @@
+//
+// Created by Mateusz M on 20/05/2026.
+//
+
+#ifndef ALGOSY2GRAFY_GRAPHTESTUTIN_HPP
+#define ALGOSY2GRAFY_GRAPHTESTUTIN_HPP
+
+#include <unordered_map>
+
+#include "BaseGraph.hpp"
+#include "GraphExceptions.hpp"
+#include "HelperFunctions.hpp"
+#include "ListGraph.hpp"
+#include "ReportMaker.hpp"
+
+//region ===================== NODE GENERATION =====================
+inline void try_generate_random_nodes(const int N, BaseGraph<int>* graph, ReportMaker* report = nullptr, std::vector<int>* generated = nullptr) {
+    for (int i = 0; i < N; ++i) { //add nodes with random values
+        const int node = rand_int(N);
+        try {
+            graph->add_node( node );
+            if (generated != nullptr) generated->push_back( node ); //won't be neede with iterators
+        } catch (const std::exception& e) {
+            if (report != nullptr) report->silent_suberror("Couldn't add node "+str(node)+": "+e.what());
+        }
+
+    }
+}
+// TODO add ability to generate any label
+inline void generate_random_nodes(const int N, BaseGraph<int> *graph) {
+    while ( graph->v() != N ) {
+        try {
+            graph->add_node( rand_int( 100 ) ); // TODO parametrize this
+        } catch ( [[maybe_unused]] NodeExistsException &e ) {}
+    }
+}
+//endregion
+
+//region ===================== EDGE GENERATION =====================
+/**
+ * Used to correctly handle exceptions when trying to add an edge to a graph by logging that exception to a report log
+ * @param edge An Edge object
+ * @param graph Pointer to Graph
+ * @param report Pointer to an optional ReportMaker object for debugging purposes (leave blank if you just want to skip the exceptions)
+ */
+inline void try_adding_edge(const Edge<int>& edge, BaseGraph<int> *graph, ReportMaker* report = nullptr) {
+    try {
+        graph->add_edge( edge );
+    }
+    catch (GraphException& e) {
+        if ( report != nullptr ) report->silent_suberror("Couldn't add edge " + str(edge.source) + "->" + str(edge.target) + ": " + e.what() );
+    }
+}
+inline void try_generate_random_edges(const int N, BaseGraph<int>* graph, ReportMaker* report) {
+    const int edges_to_add = rand_int(N) + N; // TODO improve this
+
+    for (int i = 0; i < edges_to_add; ++i) {
+        const int source = graph->random_node();
+        const int target = graph->random_node();
+        try_adding_edge(Edge(source, target, round_float( rand_float(), 2 )), graph, report);
+    }
+}
+inline void generate_random_edges(const int N, BaseGraph<int> *graph, ReportMaker* report) {
+    while ( graph->e() != N ) { // FIXME remove constants
+        const int source = graph->random_node();
+        const int target = graph->random_node();
+        try_adding_edge( Edge( source, target, round_float( rand_float(), 2 ) ), graph, report ); // TODO parametrize value function
+    }
+}
+template < HasNodes Graph >
+void fill_edges(Graph* graph, ReportMaker* r = nullptr) {
+    for ( const auto source : graph->nodes() ) {
+        for ( const auto target : graph->nodes() ) {
+            try_adding_edge(Edge<int>(source, target, rand_float()), graph, r);
+        }
+    }
+}
+template < HasNodes Graph >
+void generate_connected_graph(Graph* graph, ReportMaker* r = nullptr) {
+    bool flag = true;
+    while ( flag ) {
+        flag = false;
+        for ( const auto source : graph->nodes() ) {
+
+            int size = 0;
+            for ([[maybe_unused]] auto edge : graph->adjacents(source)) ++size;
+            // std::cout << source << ": " << size << std::endl;
+            if ( size == 0 ) {flag = true;}
+            while ( flag ) {
+                try {
+                    graph->add_edge(source, graph->random_node(), rand_float());
+                    break;
+                }
+                catch ( [[maybe_unused]] EdgeExistsException &ee ) {}
+                catch ( [[maybe_unused]] std::runtime_error &ne ) {} // TODO add custom exception for loops!!!
+            }
+        }
+    }
+}
+template < HasNodes Graph >
+void generate_dag(Graph* graph, ReportMaker* r = nullptr) {
+    std::vector<int> sorted_nodes;
+    std::copy(graph->nodes().begin(), graph->nodes().end(), std::back_inserter(sorted_nodes));
+    std::sort(sorted_nodes.begin(), sorted_nodes.end(), [&](int a, int b) { return a < b; }); // NOTE probably optional, check if works without it
+    std::cout << str(sorted_nodes) << std::endl;
+
+    for (size_t i = 0; i < sorted_nodes.size(); ++i) {
+        for (size_t j = i + 1; j < sorted_nodes.size(); ++j) {
+            if (coinflip()) {
+                auto new_edge = Edge<int>(sorted_nodes[i], sorted_nodes[j], rand_float());
+                try_adding_edge(new_edge, graph, r);
+                std::cout << "Added edge" << new_edge << std::endl;
+            }
+        }
+    }
+}
+//endregion
+
+//region ===================== GRAPH PROPERTIES =====================
+template < typename T, HasNodes Graph >
+bool is_topologically_sorted(Graph* graph, std::vector<T> test_nodes) {
+    std::unordered_map<T,int> idx;
+    for (int i = 0; i < graph->v(); ++i ) {
+        idx[test_nodes[i]] = i;
+    }
+    for (auto edge : graph->edges() ) {
+        if ( not idx[edge->source] < idx[edge->target] ) return false;
+    }
+    return true;
+}
+//endregion
+
+#endif //ALGOSY2GRAFY_GRAPHTESTUTIN_HPP
--- tests/graph_dfs_test.cpp
+++ tests/graph_dfs_test.cpp
@@ -2,78 +2,16 @@
 // Created by Mateusz M on 17/05/2026.
 //
 
+#include <format>
+#include <iostream>
+
 #include "../include/ListGraph.hpp"
 #include "../include/ReportMaker.hpp"
 #include "../include/HelperFunctions.hpp"
 #include "../include/DFS.hpp"
-
-#include <format>
-#include <iostream>
+#include "../include/GraphTestUtil.hpp"
 
 #define N 10
-
-
-
-void try_adding_edge(BaseGraph<int> *graph, const Edge<int>& edge, ReportMaker* r = nullptr) {
-    try {
-        graph->add_edge( edge );
-    }
-    catch (std::exception& e) {
-        if ( r != nullptr ) r->silent_suberror("Couldn't add edge " + str(edge.source) + "->" + str(edge.target) + ": " + e.what() );
-    }
-}
-
-void try_generate_random_edges(BaseGraph<int>* graph, ReportMaker* report = nullptr) {
-    const int edges_to_add = rand_int(N) + (N);
-
-    for (int i = 0; i < edges_to_add; ++i) {
-        const int source = graph->random_node();
-        const int target = graph->random_node();
-        try_adding_edge(graph,  Edge<int>(source, target, round_float( rand_float(), 2 )), report);
-    }
-}
-
-
-template < HasNodes Graph >
-void fill_edges(Graph* graph, ReportMaker* r = nullptr) {
-    for ( const auto source : graph->nodes() ) {
-        for ( const auto target : graph->nodes() ) {
-            try_adding_edge(graph, Edge<int>(source, target, rand_float()), r);
-        }
-    }
-}
-
-// TODO improve this function
-template < HasNodes Graph >
-void generate_connected_graph(Graph* graph, ReportMaker* r = nullptr) {
-    bool flag = true;
-    while ( flag ) {
-        flag = false;
-        for ( const auto source : graph->nodes() ) {
-            int size = 0;
-            for ([[maybe_unused]] auto edge : graph->adjacents(source)) ++size;
-
-            if ( size == 0 ) {flag = true;}
-            while ( true ) {
-                try {
-                    graph->add_edge(source, graph->random_node(), rand_float());
-                    break;
-                }
-                catch ( EdgeExistsException &ee ) {}
-                catch ( std::runtime_error &ne ) {} // TODO add custom exception for loops!!!
-            }
-        }
-    }
-}
-
-
-void generate_random_nodes(ListGraph<int> *graph) {
-    while ( graph->v() != N ) {
-        try {
-            graph->add_node( rand_int( 100 ) );
-        } catch ( [[maybe_unused]] NodeExistsException &e ) {}
-    }
-}
 
 
 void main_test(ListGraph<int>* graph) {
@@ -87,7 +25,7 @@ void main_test(ListGraph<int>* graph) {
     r.run_test("Generate a connected graph (N="+str(N)+")");
     r.log("Generating a random connected graph");
 
-    generate_random_nodes(graph);
+    generate_random_nodes(N, graph);
     generate_connected_graph(graph);
 
     r.log("Saving graph to json");
