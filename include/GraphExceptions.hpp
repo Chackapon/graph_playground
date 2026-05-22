411f4b79293fcc96329b5c4f5cf6c037eb5c8430
Chackapon <mateusz.miliutin@gmail.com>
2026-05-22 20:45:21 +0200

    - Reafctoring - New LoopsInSimpleGraph exception

--- include/GraphExceptions.hpp
+++ include/GraphExceptions.hpp
@@ -14,38 +14,58 @@ class GraphException : public std::runtime_error
     explicit GraphException(const std::string& error_msg) : runtime_error( "Graph Exception: " + error_msg ) {}
 };
 
+//region ===================== NODES =====================
+class NodeException : public GraphException
+{
+public:
+    explicit NodeException(const std::string& error_msg) : GraphException(error_msg) {}
+};
 
-class NodeExistsException : public GraphException
+class NodeExistsException : public NodeException
 {
     public:
-    explicit NodeExistsException() : GraphException("node already exists") {}
+    explicit NodeExistsException() : NodeException("node already exists") {}
 };
+//endregion
 
-
-class EdgeExistsException : public GraphException
+//region ===================== EDGES =====================
+class EdgeException : public GraphException
 {
 public:
-    explicit EdgeExistsException() : GraphException("edge already exists") {}
+    explicit EdgeException(const std::string& error_msg) : GraphException(error_msg) {}
 };
 
-
-class EdgeDoesntExistException : public GraphException
+class LoopsInSimpleGraph : public EdgeException
 {
 public:
-    explicit EdgeDoesntExistException() : GraphException("edge doesn't exist") {}
+    explicit LoopsInSimpleGraph() : EdgeException("loop edges are not allowed in a simple graph") {}
 };
 
-class TargetDoesntExistException : public GraphException
+class EdgeExistsException : public EdgeException
 {
 public:
-    explicit TargetDoesntExistException() : GraphException("target node doesn't exist") {}
+    explicit EdgeExistsException() : EdgeException("edge already exists") {}
 };
 
-class SourceDoesntExistException : public GraphException
+
+class EdgeDoesntExistException : public EdgeException
 {
 public:
-    explicit SourceDoesntExistException() : GraphException("source node doesn't exist") {}
+    explicit EdgeDoesntExistException() : EdgeException("edge doesn't exist") {}
 };
+
+class TargetDoesntExistException : public EdgeException
+{
+public:
+    explicit TargetDoesntExistException() : EdgeException("target node doesn't exist") {}
+};
+
+class SourceDoesntExistException : public EdgeException
+{
+public:
+    explicit SourceDoesntExistException() : EdgeException("source node doesn't exist") {}
+};
+//endregion
 
 
 #endif //ALGOSY2GRAFY_GRAPHEXCEPTIONS_HPP
